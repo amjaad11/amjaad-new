02fe428597a18668c4e047ac45399607e0222713
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-22 22:24:48 +0300

    Add files via upload

---
+++ TheNumbersPositiveORNegative.cpp/TheNumbersPositiveORNegative.cpp/TheNumbersPositiveORNegative.cpp.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	int mark = 1;
+ (mark > 0) ?  cout << "positive" : (mark == 0) ?  cout <<  "Zero" : cout  << "Negative";
+	
+
+
+	return 0;
+}
