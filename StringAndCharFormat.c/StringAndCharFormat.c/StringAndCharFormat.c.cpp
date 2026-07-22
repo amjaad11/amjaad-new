551bac54dcd2c8331b5beabe1eb5a53d30171a39
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-22 22:32:48 +0300

    Add files via upload

---
+++ StringAndCharFormat.c/StringAndCharFormat.c/StringAndCharFormat.c.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+
+
+	cout << "__________|_________________________________|_______|" << endl;
+	cout << "  Code    |                Name             |  Mark |" << endl;
+	cout << "__________|_________________________________|_______|" << endl;
+
+
+	cout << setw(10) << "C101" << "|" << setw(33) << "introduction to programming 1" << "|" << setw(8) << "|" << endl;
+	cout << setw(10) << "C102" << "|" << setw(33) << "Computer Hardware" << "|" << setw(7) << "88" << "|" << endl;
+	cout << setw(10) << "C1035243" << "|" << setw(33) << "Network" << "|" << setw(7) <<"75" << "|" << endl;
+	cout << "__________|_________________________________|_______|" << endl;
+};
+
+
