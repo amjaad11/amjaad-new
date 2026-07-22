8ff9e5ef9af4e6174bcb85aa62b3b80657eeb3c7
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-22 22:49:05 +0300

    Add files via upload

---
+++ RemoveElements.c/RemoveElements.c/RemoveElements.c.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    vector <int> vNumbers;
+    vNumbers.push_back(10);
+    vNumbers.push_back(20);
+    vNumbers.push_back(30);
+    vNumbers.push_back(40);
+    vNumbers.push_back(50);
+    cout << "Stack Size=" << vNumbers.size() << endl;
+
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    vNumbers.pop_back();
+    cout << "Stack Size=" << vNumbers.size() << endl;
+
+    if (vNumbers.size() > 0)
+    {
+        vNumbers.pop_back();
+    }
+
+    return 0;
+}
+
