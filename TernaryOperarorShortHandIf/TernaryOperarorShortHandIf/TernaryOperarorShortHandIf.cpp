11fbcc3d170678adb635094a54df53c3e68bcd40
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-22 22:28:02 +0300

    Add files via upload

---
+++ TernaryOperarorShortHandIf/TernaryOperarorShortHandIf/TernaryOperarorShortHandIf.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	int mark = 44;
+
+	(mark >= 50) ? cout << "Pass" : (mark >= 50) ? cout << "Pass" : cout << "Fail";
+	
+
+	return 0;
+}
---
+++ TernaryOperarorShortHandIf/TernaryOperarorShortHandIf/theNumberisPositiveorNegative.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	int mark = 1;
+
+	(mark >= 1) ? cout << "positive" : cout << "Fail";
+
+
+
+	return 0;
+};
