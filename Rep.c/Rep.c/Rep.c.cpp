6826088496335f59151306c19fd091e5ccb578e8
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-22 22:46:28 +0300

    Add files via upload

---
+++ Rep.c/Rep.c/Rep.c.cpp
@@ -0,0 +1,20 @@
+﻿﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string original = "Welcome to Jordan, Jordan is a nice country";
+
+    size_t pos = original.find("jordan");
+
+    if (pos != string::npos)
+    {
+        original =
+            original.substr(0, pos)
+            + "the USA"
+            + original.substr(pos + 6);
+    }
+
+    cout << original;
+}
