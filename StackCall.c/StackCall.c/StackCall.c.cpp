46d711a96c2ec201a3c655febfaee2862e24517f
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-22 22:36:55 +0300

    Add files via upload

---
+++ StackCall.c/StackCall.c/StackCall.c.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int Mypower(int Base, int Power)
+{
+    
+   if (Power == 0)
+	   return 1;
+   else
+   {
+       return (Base * Mypower(Base, Power - 1));
+
+   }
+    
+}
+int main()
+{
+   cout << Mypower(2, 4);
+  
+   return 0;
+
+
+}
+
+
+
