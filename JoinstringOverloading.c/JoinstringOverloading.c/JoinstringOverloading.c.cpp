3112be01f39cd3d50ab64180cb2c30e0e57f3837
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-23 12:28:16 +0300

    Add files via upload

---
+++ JoinstringOverloading.c/JoinstringOverloading.c/JoinstringOverloading.c.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <vector>
+
+using namespace std;
+
+void Join()
+{
+	cout << "Vector after Join: \n";
+	string words[3] = {"amjaad","hassna" , "asd"};
+	
+		cout << words << endl;
+}
+int main()
+{
+	Join();
+
+}
