c38612e87b6a53f3b01c3bffd8ec525fff054231
fatma <97044328+amjaad11@users.noreply.github.com>
2026-07-23 13:14:59 +0300

    Add files via upload

---
+++ ConsoleApplication2.c/ConsoleApplication2.c/ConsoleApplication2.c.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string> 
+using namespace std;
+string replaceWord(string original, string oldWord, string newWord, bool matchCase) {
+	string result = original;
+	int pos = 0;
+	while (true) {
+		if (matchCase)
+		{
+			pos = result.find(oldWord, pos);
+		}
+
+		else
+		{
+		string lowerResult = result;
+		string lowerWord = oldWord;
+		for (int i = 0; i < lowerResult.length(); i++)
+			lowerResult[i] = tolower(lowerResult[i]);
+
+        for (int i = 0; i < lowerWord.length(); i++)
+
+	lowerWord[i] = tolower(lowerWord[i]);
+pos = lowerResult.find(lowerWord, pos); 
+		}
+		if (pos == string::npos)
+			break;
+		result = result.substr(0, pos) + newWord + result.substr(pos + oldWord.length());
+		pos += newWord.length();
+	}
+	return result;
+}
+int main() {
+	string original = "Welcome to Jordan , Jordan is a nice country";
+	cout << "Original String:" << endl;
+	cout << original << endl << endl;
+	cout << "Replace with match case:" << endl;
+	cout << replaceWord(original, "jordan", "USA", true) << endl << endl;
+	cout << "Replace with dont match case:" << endl;
+	cout << replaceWord(original, "jordan", "USA", false) << endl;
+	return 0;
+}
